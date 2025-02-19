a33c28db5333fcf4efc3d473aca25af16e9fc728
IvanTarasov <iwan.tarasow2013.gmail.com>
2025-02-19 22:02:03 +0300

    fixed sortTest

--- lab1/src/test/java/org/example/sort/SortsTest.java
+++ lab1/src/test/java/org/example/sort/SortsTest.java
@@ -2,63 +2,51 @@ package org.example.sort;
 
 import com.sun.management.OperatingSystemMXBean;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
 import java.lang.management.ManagementFactory;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SortsTest {
 
-    PrintStream originalOut;
     final OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-
-    ByteArrayOutputStream getOutputStream() {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        originalOut = System.out;
-        System.setOut(new PrintStream(outputStream));
-        return outputStream;
-    }
-
-    Path getPathFromResources(String path) throws URISyntaxException {
-        return Path.of(getClass().getClassLoader().getResource(path).toURI());
-    }
 
     @AfterEach
     void tearDown() throws InterruptedException {
         Thread.sleep(500);
-        if (originalOut != null)
-            System.setOut(originalOut);
         System.out.printf("CPU load: %.3f%n", osBean.getProcessCpuLoad() * 100);
     }
 
     static Stream<Arguments> selectionSortInputs() {
         return Stream.of(
-                Arguments.of("sortTest1.txt", new Integer[]{5, 3, 10, 5, 2, 57, 44, 1}),
-                Arguments.of("sortTest2.txt", new Double[]{4.0, 2.1, 5.96, 5.96, 0.0, -0.4}),
-                Arguments.of("sortTest3.txt", new Integer[]{4, 4, 4, 4}),
-                Arguments.of("sortTest4.txt", new String[]{"Chocolate", "Work", "Adidas", "Volga", "Ivan"})
+                Arguments.of((Object) new Integer[]{5, -3, 10, 8, 2, 57, 44, 1}),   // Разнообразный набор целых чисел
+                Arguments.of((Object) new Integer[]{4, 4, 4, 4}),                          // Массив из одинаковых элементов
+                Arguments.of((Object) new Integer[]{9, 8, 7, 6, 5}),                       // Массив в обратном порядке (худший случай)
+                Arguments.of((Object) new Integer[]{-1, 0, 1, 2, 3, 4}),                   // Отсортированный массив
+                Arguments.of((Object) new Integer[]{}),                                    // Пустой массив
+                Arguments.of((Object) new Integer[]{42}),                                  // Массив из одного элемента
+                Arguments.of((Object) new Double[]{4.0, 2.1, 5.96, 5.96, 0.0, -0.4}),      // Массив типа Double
+                Arguments.of((Object) new Double[]{Double.MIN_VALUE, 0.0, Double.MAX_VALUE}),   // Массив с экстремальными значениями
+                Arguments.of((Object) new String[]{"Chocolate", "Work", "Adidas", "Volga", "Ivan"}) // Массив строк
         );
     }
 
     @ParameterizedTest
     @MethodSource("selectionSortInputs")
-    <T extends Comparable<T>> void selectionSortTest(String filepath, T[] array) throws URISyntaxException, IOException {
-        String excepted = Files.readString(getPathFromResources(filepath));
-        ByteArrayOutputStream outputStream = getOutputStream();
+    <T extends Comparable<T>> void selectionSortTest(T[] array) {
         Sorts.selectionSort(array);
-        assertEquals(excepted, outputStream.toString());
+        assertTrue(IntStream.range(0, array.length - 1).allMatch(i -> array[i + 1].compareTo(array[i]) >= 0));
+    }
+
+    @Test
+    void selectionSortTestNull() {
+        assertThrows(NullPointerException.class, () -> Sorts.selectionSort(null));
     }
 }
